b4eecc06009bba8f591abdeb3f90fc501b679d6c
Michael Kim <mkim0407@gmail.com>
2019-08-02 20:52:13 -0700

    add basic implementation

--- flake8_use_fstring/format.py
+++ flake8_use_fstring/format.py
@@ -1,10 +1,35 @@
+import token as _token
+import typing as _typing
+from tokenize import (
+    TokenInfo as _TokenInfo,
+)
+
+
 class StrFormatDetector(object):
     name = 'use-fstring-format'
     version = '1.0'
 
-    def __init__(self, logical_line, tokens):
+    def __init__(
+            self,
+            logical_line: str,
+            tokens: _typing.List[_TokenInfo],
+    ):
         self.logical_line = logical_line
         self.tokens = tokens
 
-    def __iter__(self):
-        yield 0, 'FS002 test'
+    @staticmethod
+    def match(token: _TokenInfo, last_token: _TokenInfo) -> bool:
+        if token.exact_type != _token.NAME:
+            return False
+        if token.string != 'format':
+            return False
+        if last_token.exact_type != _token.DOT:
+            return False
+        return True
+
+    def __iter__(self) -> _typing.Iterator[_typing.Tuple[int, str]]:
+        last_token = None
+        for token in self.tokens:
+            if self.match(token, last_token):
+                yield last_token.start, f"FS002 '.format' used"
+            last_token = token
--- flake8_use_fstring/percent.py
+++ flake8_use_fstring/percent.py
@@ -1,10 +1,24 @@
+import token as _token
+import typing as _typing
+from tokenize import (
+    TokenInfo as _TokenInfo,
+)
+
+
 class PercentFormatDetector(object):
     name = 'use-fstring-percent'
     version = '1.0'
 
-    def __init__(self, logical_line, tokens):
+    def __init__(
+            self,
+            logical_line: str,
+            tokens: _typing.List[_TokenInfo],
+    ):
         self.logical_line = logical_line
         self.tokens = tokens
 
-    def __iter__(self):
-        yield 0, 'FS001 test'
+    def __iter__(self) -> _typing.Iterator[_typing.Tuple[int, str]]:
+        for token in self.tokens:
+            if token.exact_type != _token.PERCENT:
+                continue
+            yield token.start, f"FS001 '%' operator used"
